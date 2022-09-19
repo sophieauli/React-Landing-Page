b104af834ff853ff541d347d28d6262f5021308e
Sophie Aulitzky <sophie.aulitzky@gmail.com>
2022-09-19 19:05:36 +0200

    Work Day One Part One

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+/** @jsxImportSource @emotion/react */
 
-function App() {
+import { css } from '@emotion/react';
+import logo from './images/doo_logo.svg';
+
+const mainStyles = css``;
+
+const headerStyles = css`
+  display: flex;
+  justify-content: space-between;
+  padding: 23px 61px;
+  position: fixed;
+  width: 100%;
+  z-index: 1;
+`;
+const downloadButton = css`
+  color: #6e6d7a;
+  background: #f5f6f9;
+  border-radius: 8px;
+  padding: 10px 40px;
+  font-family: inherit;
+`;
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div css={mainStyles}>
+      <header css={headerStyles}>
+        <a class="logo-link" href="/" title="Go back to the Doo homepage">
+          <img src={logo} alt="Doo Logo" />
         </a>
+
+        <nav>
+          <a href="#abc">DOO FOR IOS</a>
+          <a href="#abc">DOO FOR MAC</a>
+          <a href="#abc">SUPPORT</a>
+          <a href="#abc" css={downloadButton}>
+            DOWNLOAD
+          </a>
+        </nav>
       </header>
     </div>
   );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
+import { css, Global } from '@emotion/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    <Global
+      styles={css`
+        *,
+        *::before,
+        *::after {
+          box-sizing: border-box;
+        }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+        body {
+          margin: 0;
+          background: white;
+          font-family: 'Avenir Next', -apple-system, BlinkMacSystemFont,
+            'Helvetica Neue', 'Helvetica', 'Helvetica', 'Gill Sans', sans-serif;
+        }
+      `}
+    />
+    <App />
+  </React.StrictMode>,
+);
+// ('reportWebVitals');
